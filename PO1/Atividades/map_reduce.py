5ab996301d3a46670dfc9c052da135a6f844feba
MarcosWillianAP1K <mwss@outlook.com>
2024-10-11 10:26:19 -0300

    ok

--- PO1/Atividades/map_reduce.py
+++ PO1/Atividades/map_reduce.py
@@ -6,3 +6,9 @@ l= [1,2,3,4,5,6]
 resultado = ft.reduce(lambda x, y: x + y, map(lambda x: x * 2, filter(lambda x: x % 2 == 0, l)))
 
 print(resultado)
+
+lista = [[[[[[[[[[[[[["bah"]]]]]]]]]]]]]]
+
+print(lista[0][0][0][0][0][0][0][0][0][0][0][0][0][0])
+
+print(lista)
